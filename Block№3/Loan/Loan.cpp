4984111ff83ef7d8d4f1259c72ae4aa0d36bf799
PaleLuna <robertmkrtyan334@gmail.com>
2021-10-06 21:19:35 +0300

    Added task№2 'loan'

---
+++ Block№3/Loan/Loan.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	//Объявление переменных
+	double s; //Размер
+	double m; //
+	unsigned int n; //
+
+	//Инициализация переменных
+
+	system("pause>nul");
+	return 0;
+}
