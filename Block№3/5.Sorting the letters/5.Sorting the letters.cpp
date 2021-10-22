24df6b4d2247192359d6c2fc40353a9e4cd2f9c1
PaleLuna <robertmkrtyan334@gmail.com>
2021-10-22 17:12:08 +0300

    Completed

--- Block№3/5.Sorting the letters/5.Sorting the letters.cpp
+++ Block№3/5.Sorting the letters/5.Sorting the letters.cpp
@@ -1,1 +1,64 @@
-﻿
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void QuickSort(char* text, int start, int end)
+{
+	int leftHold = start;
+	int rightHold = end;
+	char pivot = text[(start+end)/2];
+
+	
+
+	while (leftHold < rightHold)
+	{
+		while (tolower(text[leftHold]) < tolower(pivot))
+			leftHold++;
+		while (tolower(text[rightHold]) > tolower(pivot))
+			rightHold--;
+
+
+		if (leftHold < rightHold)
+		{
+			swap(text[leftHold++], text[rightHold--]);
+		}
+	}
+	
+	if (start < leftHold)
+	{
+		QuickSort(text, start, leftHold-1);
+	}
+	if (rightHold < end)
+	{
+		QuickSort(text, rightHold+1, end);
+	}
+
+
+}
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	//Объявление переменных
+	const int count = 12;
+	char text[count];
+
+	for (int i = 0; i < count; i++)
+	{
+		cin >> text[i];
+	}
+	cin.ignore(512, '\n');
+
+	QuickSort(text, 0, count-1);
+
+	for (int i = 0; i < count; i++)
+	{
+		printf("%c", text[i]);
+	}
+	
+	printf("\n");
+	system("pause");
+	return 0;
+}
