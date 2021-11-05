3d9fc5e79864e6ca2b1441656494ac87eefd407e
PaleLuna <robertmkrtyan334@gmail.com>
2021-11-05 14:24:22 +0300

    Task№5 completed

---
+++ Block№4/5.Sine graph/5.Sine graph.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+
+using namespace std;
+
+const double pi = 3.14159265358979323846;
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	const int offset = 14;
+	const int amplitude = offset;
+	const int frequency = 18;
+
+	COORD position2D;
+
+	for (int x = 0; x < 109; x++)
+	{
+		position2D.X = x;
+		position2D.Y = -(int)(amplitude * sin(pi/ frequency * x)) + offset;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position2D);
+		printf("*");
+	}
+
+	system("pause>nul");
+	return 0;
+}
