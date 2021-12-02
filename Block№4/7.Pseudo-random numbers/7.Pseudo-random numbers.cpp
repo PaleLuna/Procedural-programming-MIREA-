e28c87d2624528b3227dc0bd8e5ad488df5f4178
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2021-12-02 17:50:01 +0300

    Update 7.Pseudo-random numbers.cpp

--- Block№4/7.Pseudo-random numbers/7.Pseudo-random numbers.cpp
+++ Block№4/7.Pseudo-random numbers/7.Pseudo-random numbers.cpp
@@ -1,44 +1,55 @@
-﻿#include <iostream>
+#include <iostream>
 #include <Windows.h>
 
 using namespace std;
 
 
 
-int GeneratorOfNumbers(int s, int i, int m, int a, int c);
+int GeneratorOfNumbers(int* array, int s, int i, int m, int a, int c);
 
 int main()
 {
 	system("chcp 1251>nul");
-	
+
 	//Объявление переменных
 	int s = 50;
 	int Start = 3;
 	int End = 100;
-	int m = 37; 
-	int c = -64; 
+	int m = 37;
+	int c = -64;
 	int a = 5;
-	int i = End - Start;
+	int count = End - Start;
 
-
-	int result = GeneratorOfNumbers(s, i, m, a, c);
-
+	int* array = new int[count];
 	printf("Генератор псевдослучайных чисел\n\n");
-	
-	printf("%i\n", result);
-	
+	GeneratorOfNumbers(array, s, count, m, a, c);
+
+	for (int j = 0; j < count; j++)
+	{
+		printf("%-2i:%-4i| ", j, array[j]);
+		if ((j != 0) && (j % 10 == 0))
+		{
+			printf("\n");
+		}
+
+	}
+
+
+	//printf("%i\n", result);
+
 
 
 	system("pause>nul");
 	return 0;
 }
 
-int GeneratorOfNumbers(int s, int i, int m, int a, int c)
+int GeneratorOfNumbers(int* array, int s, int i, int m, int a, int c)
 {
 	if (i <= 0)
 	{
 		return s;
 	}
-	s += (m * (GeneratorOfNumbers(s, --i, m, a, c)) + a) % c;
+	s += (m * (GeneratorOfNumbers(array, s, --i, m, a, c)) + a) % c;
+	array[i] = s;
 	return s;
 }
