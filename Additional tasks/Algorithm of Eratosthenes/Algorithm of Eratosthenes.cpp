807e8698e60035a9c22467170a5987ba2256b709
PaleLuna <robertmkrtyan334@gmail.com>
2021-12-03 15:10:44 +0300

    Algorithm of Eratosthenes

---
+++ Additional tasks/Algorithm of Eratosthenes/Algorithm of Eratosthenes.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	printf("Реализация алгоритма Эратосфена\n\n\n");
+
+	//Объявление и иницализация переменных
+	int len = 0;
+	int* arrayOfNums;
+
+	printf("Введите натуральное число, соответствующее диапозону простых чисел: ");
+	cin >> len;
+
+	arrayOfNums = new int[++len];
+
+	for (int i = 0; i < len; i++)
+	{
+		arrayOfNums[i] = i;
+	}
+	arrayOfNums[1] = 0;
+
+	for (int i = 2; i < len; i++)
+	{
+		if (arrayOfNums[i] != 0)
+		{
+			for (int j = i*i; j < len; j += i)
+			{
+				arrayOfNums[j] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		if (arrayOfNums[i] != 0)
+		{
+			printf("%i\n", arrayOfNums[i]);
+		}
+	}
+
+
+
+
+	delete[] arrayOfNums;
+	system("pause");
+	return 0;
+}
