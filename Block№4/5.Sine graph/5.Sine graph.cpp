80b8d89847284ec9e752287565bec1472cfe481f
PaleLuna <robertmkrtyan334@gmail.com>
2021-12-16 22:41:01 +0300

    BF

--- Block№4/5.Sine graph/5.Sine graph.cpp
+++ Block№4/5.Sine graph/5.Sine graph.cpp
@@ -11,15 +11,15 @@ int main()
 	system("chcp 1251>nul");
 
 	const int offset = 14;
-	const int amplitude = offset;
-	const int frequency = 18;
+	const int amplitude = offset-2;
+	const float frequency = 1.2;
 
 	COORD position2D;
 
-	for (int x = 0; x < 109; x++)
+	for (int x = 0; x < 110; x++)
 	{
 		position2D.X = x;
-		position2D.Y = -(int)(amplitude * sin(pi / x)) + offset;
+		position2D.Y = -(int)(amplitude * sin((x/ pi)*frequency)) + offset;
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position2D);
 		printf("*");
 	}
